f7be712435d148697ce9c0157a0549323cf725a6
songhwee1 <songhwee1@naver.com>
2022-07-28 22:23:41 +0900

    check id exists

--- db/query.js
+++ db/query.js
@@ -1,2 +1,21 @@
 const db = require('./dbConnection.js');  // use mysql db connection
 const con = db.init();
+
+function checkIdExists(id, callback) {
+  const SQL = `SELECT EXISTS(SELECT id FROM user WHERE id = ?) AS exist`
+  const values = [id];
+
+  con.query(SQL, values, (err, result, field) => {
+    if (err) {
+      console.log(err);
+      callback('err');
+    }
+    else {
+      callback(result);
+    }
+  });
+}
+
+module.exports = {
+  checkIdExists
+}
