4f39ef6cf777d74227f9a2204be5cd09bf5625bb
songhwee1 <songhwee1@naver.com>
2022-08-05 20:34:50 +0900

    fix get body to query params

---
+++ routes/job/index.js
@@ -0,0 +1,17 @@
+// launch modules
+module.exports = (express, db) => {
+  const router = express.Router({ mergeParams: true });
+
+  /**
+   * @description job main page
+   * @method get
+   * @returns job db data
+   */
+  router.get("/", (req, res) => {
+    db.loadPostList('job', req.query.category, null, (result) => {
+      res.send(result)
+    })
+  });
+
+  return router;
+}
