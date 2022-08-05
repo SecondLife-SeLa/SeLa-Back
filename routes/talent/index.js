66fed3fa8fa581e8ef5b4c34eb4bf16e8a26c73e
songhwee1 <songhwee1@naver.com>
2022-08-05 20:32:58 +0900

    connect to aws s3 bucket

--- routes/talent/index.js
+++ routes/talent/index.js
@@ -1,7 +1,25 @@
 // launch modules
-module.exports = (express, db) => {
+module.exports = (express, db, multer, multerS3, s3) => {
   const router = express.Router({ mergeParams: true });
 
+  const talent_img_upload = multer({
+    // use multer module to save image from symptom img upload
+    storage: multerS3({
+      s3: s3,
+      bucket: "second_life_imgs/talent_img_upload",
+      acl: "public-read",
+      key: function (req, file, cb) {
+        cb(
+          null,
+          Math.floor(Math.random() * 1000).toString() +
+            Date.now() +
+            "." +
+            file.originalname.split(".").pop()
+        );
+      },
+    }),
+  });
+  
   /**
    * @description talent main page
    * @method get
@@ -11,6 +29,15 @@ module.exports = (express, db) => {
     db.loadPostList('talent', req.body.category, req.body.page, (result) => {
       res.send(result)
     })
+  });
+
+  /**
+   * @description talent write page
+   * @method post
+   * @returns status code
+   */
+  router.get("/write", (req, res) => {
+    // have to write code here
   });
 
   return router;
