cbb3f9b8ce3c13c88c3b309d3bd10594d33fd4f0
SongHweeismyteacher <chskj10@naver.com>
2022-09-10 23:28:08 +0900

    로그인, 로그아웃 라우터

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-module.exports = (express, session, sessionStore) => {
+module.exports = (express) => {
 
   const router = express.Router({ mergeParams: true });
 
   router.get('/', function(req, res){
     res.send('루트입니다.')
   });
-  
+
   return router;
 }
   
